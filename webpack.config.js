2ae4072855406d4f4254cb662d96598bcd186ae3
donghyuk <gohyuk528@gmail.com>
2021-11-10 22:55:10 +0900

    initial setting

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const config = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["styled-components"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
+
+module.exports = config;
