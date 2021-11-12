82a128094af3ce673bb4a4fd8ac8d27df2be6dd7
donghyuk <gohyuk528@gmail.com>
2021-11-13 01:17:46 +0900

    [ADD] search github issues function to search bar button

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
+
 import styled from "styled-components";
 import { flexCenter, fullWidthAndHeight } from "../../styles/mixin";
+
 import Button from "../Shared/Button";
+import { setIssues } from "../../redux/issueReducer";
+import { searchGithubIssues } from "../../api/github";
 
 function SearchBar() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [inputURL, setInputURL] = useState("");
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+
+    const data = await searchGithubIssues(inputURL);
+
+    if (data.message === "Not Found") {
+      setInputURL("");
+    }
+
+    const [, , , owner, repository] = inputURL.split("/");
+
+    dispatch(setIssues(data));
+    history.push(`/${owner}/${repository}/issues`);
+  };
+
   return (
     <Wrapper>
       <label htmlFor="search">
-        <Input id="search" autoComplete="off" placeholder="Enter Repository URL" />
+        <Input
+          id="search"
+          value={inputURL}
+          autoComplete="off"
+          placeholder="Enter Repository URL"
+          onChange={(event) => setInputURL(event.target.value)}
+          required
+        />
       </label>
-      <SearchButton>Search</SearchButton>
+      <SearchButton handleClick={handleClick}>Search</SearchButton>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.form`
   ${flexCenter}
   height: 50px;
 
