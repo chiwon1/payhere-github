47283d7b67dd3cacac4f830153693e3c862d634a
donghyuk <gohyuk528@gmail.com>
2021-11-14 18:51:59 +0900

    [ADD] cover fetch bookmark issues function with thunk

--- src/store/issue/issuesSlice.js
+++ src/store/issue/issuesSlice.js
@@ -1,11 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { searchGithubIssues } from "../../api/github";
+import { searchBookmarkIssues, searchGithubIssues } from "../../api/github";
 
 export const fetchIssues = createAsyncThunk("issue/fetchIssues", async ({ url, page }) => {
   const res = await searchGithubIssues(url, page);
 
   return { ...res, page };
 });
+
+export const fetchBookmarkIssues = createAsyncThunk(
+  "issue/fetchBookmarkIssues",
+  async ({ url, page }) => {
+    const res = await searchBookmarkIssues(url, page);
+
+    return { ...res, page };
+  }
+);
 
 export const issuesSlice = createSlice({
   name: "issue",
@@ -15,7 +24,14 @@ export const issuesSlice = createSlice({
     error: null,
     issuesLength: 0,
   },
-  reducers: {},
+  reducers: {
+    resetState: (state) => {
+      state.error = null;
+      state.page = 1;
+      state.issuesLength = 0;
+      state.list = [];
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchIssues.fulfilled, (state, action) => {
@@ -29,7 +45,17 @@ export const issuesSlice = createSlice({
       .addCase(fetchIssues.rejected, (state, action) => {
         state.error = action.error;
       });
+    builder.addCase(fetchBookmarkIssues.fulfilled, (state, action) => {
+      const data = action.payload;
+
+      state.error = null;
+      state.list = data.items;
+      state.page = data.page;
+      state.issuesLength = data.total_count;
+    });
   },
 });
+
+export const { resetState } = issuesSlice.actions;
 
 export default issuesSlice.reducer;
